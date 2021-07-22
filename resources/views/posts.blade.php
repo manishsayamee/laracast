60224ee5b9b0168e9be4d0e62974760960ad3a40
manishsayamee <manishsayamee@gmail.com>
2021-07-22 16:43:24 +0545

    convert HTML and Css to blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,24 @@
 <x-layout>
 
+@include('_posts-header')
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+          <x-card-featured-post />
+
+            <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+
+            </div>
+
+            <div class="lg:grid lg:grid-cols-3">
+   
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+
+            </div>
+        </main>
+<!-- 
 <x-slot">
 @foreach ($posts as $post) 
 
@@ -19,5 +38,5 @@
 
 
 </article>
-@endforeach
+@endforeach -->
 </x-layout>
